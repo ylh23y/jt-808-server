c2c6e5a26f1d973d050d19fe0025263c3403be34
ylh23y126 <359639297@qq.com>
2022-03-25 13:18:43 +0800

    Consts

---
+++ src/main/java/com/yun/jt808/common/WebConsts.java
@@ -0,0 +1,15 @@
+package com.yun.jt808.common;
+/**
+* @Description: HTTP 请求code 
+* @author James
+* @date 2021年10月15日 下午12:35:16
+ */
+public class WebConsts {
+
+	/**
+	 * 查询定位信息
+	 */
+	public static final Integer LOCATION_QUERY = 0x8201;
+	
+	public static final Integer SEND_TERMINAL_UPGRADE_PACKAGE = 0x8108;
+}
